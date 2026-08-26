d95fc89ef88043937e2f21e69b8f008527ff9f97
ReCloudFox <ReCloudFox@users.noreply.github.com>
2026-08-26 10:27:24 +0000

    INOI_A75_Elegance: Add extras
    for sys_mssi_64_cn_armv82-user 14 UP1A.231005.007 51023 release-keys
    
    Signed-off-by: ReCloudFox <ReCloudFox@users.noreply.github.com>

---
+++ join_split_files.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary64/TrichromeLibrary64.apk.* 2>/dev/null >> product/app/TrichromeLibrary64/TrichromeLibrary64.apk
+rm -f product/app/TrichromeLibrary64/TrichromeLibrary64.apk.* 2>/dev/null
+cat product/app/WebViewGoogle64/WebViewGoogle64.apk.* 2>/dev/null >> product/app/WebViewGoogle64/WebViewGoogle64.apk
+rm -f product/app/WebViewGoogle64/WebViewGoogle64.apk.* 2>/dev/null
+cat product/app/Meet/Meet.apk.* 2>/dev/null >> product/app/Meet/Meet.apk
+rm -f product/app/Meet/Meet.apk.* 2>/dev/null
+cat product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null >> product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk
+rm -f product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/priv-app/Messages/Messages.apk.* 2>/dev/null >> product/priv-app/Messages/Messages.apk
+rm -f product/priv-app/Messages/Messages.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null >> product/priv-app/Phonesky/Phonesky.apk
+rm -f product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/GoogleDialer/GoogleDialer.apk.* 2>/dev/null >> product/priv-app/GoogleDialer/GoogleDialer.apk
+rm -f product/priv-app/GoogleDialer/GoogleDialer.apk.* 2>/dev/null
+cat system_ext/app/MiraVision/MiraVision.apk.* 2>/dev/null >> system_ext/app/MiraVision/MiraVision.apk
+rm -f system_ext/app/MiraVision/MiraVision.apk.* 2>/dev/null
+cat system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system_ext/apex/com.android.vndk.v30.apex
+rm -f system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system_ext/priv-app/MtkSettings/MtkSettings.apk.* 2>/dev/null >> system_ext/priv-app/MtkSettings/MtkSettings.apk
+rm -f system_ext/priv-app/MtkSettings/MtkSettings.apk.* 2>/dev/null
+cat system/system/app/paw-rumble-release-production/paw-rumble-release-production.apk.* 2>/dev/null >> system/system/app/paw-rumble-release-production/paw-rumble-release-production.apk
+rm -f system/system/app/paw-rumble-release-production/paw-rumble-release-production.apk.* 2>/dev/null
+cat system/system/app/SXS_Camera/SXS_Camera.apk.* 2>/dev/null >> system/system/app/SXS_Camera/SXS_Camera.apk
+rm -f system/system/app/SXS_Camera/SXS_Camera.apk.* 2>/dev/null
+cat system/system/app/Blibli-11.6.0_66615.apk.* 2>/dev/null >> system/system/app/Blibli-11.6.0_66615.apk
+rm -f system/system/app/Blibli-11.6.0_66615.apk.* 2>/dev/null
+cat system/system/apex/com.android.virt.apex.* 2>/dev/null >> system/system/apex/com.android.virt.apex
+rm -f system/system/apex/com.android.virt.apex.* 2>/dev/null
